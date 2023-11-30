e27b0893df5dcc0f940151d0d28889f263a63f28
Edison Matos <edison.matos@live.com>
2023-11-30 14:30:24 -0300

    feat(mvc): Create Controller

--- src/server.js
+++ src/server.js
@@ -6,12 +6,10 @@ const app = express();
 app.use(express.json());
 app.use(router);
 
-app.get("/", (req, res) => {
-  res.status(200).send({ status: "ok" });
+app.get("/", (request, response) => {
+  response.status(200).send({ status: "ok" });
 });
 
 app.listen(3000, () => {
-  console.log(
-    "Server is runing ok. Check https://recep10-expressjs.onrender.com/"
-  );
+  console.log("Server is runing ok. Check http://localhost:3000");
 });

07c3b25903a4dd30646c37a19951f3ea8d51559b
EdisonMatos <edison.matos@live.com>
2023-12-01 13:50:26 -0300

    feat(nameRoute): Implement full crud

--- src/controllers/nameController.js
+++ src/controllers/nameController.js
@@ -7,6 +7,16 @@ class NameController {
     return response.status(200).json(findAll);
   }
 
+  async findOne(request, response) {
+    const { id } = request.params;
+    const userFound = await prismaClient.testName.findFirst({
+      where: {
+        id,
+      },
+    });
+    return response.status(200).json(userFound);
+  }
+
   async create(request, response) {
     const { name } = request.body;
     const newUser = await prismaClient.testName.create({
@@ -16,6 +26,32 @@ class NameController {
     });
 
     return response.status(201).json(newUser);
+  }
+
+  async update(request, response) {
+    const { id } = request.params;
+    const { name } = request.body;
+    const updatedUser = await prismaClient.testName.update({
+      data: {
+        name,
+      },
+      where: {
+        id,
+      },
+    });
+
+    return response.status(200).json(updatedUser);
+  }
+
+  async delete(request, response) {
+    const { id } = request.params;
+    const deletedUser = await prismaClient.testName.delete({
+      where: {
+        id,
+      },
+    });
+
+    return response.status(200).json(deletedUser);
   }
 }
 
--- src/routers/nameRouter.js
+++ src/routers/nameRouter.js
@@ -6,6 +6,9 @@ const nameRouter = Router();
 const nameController = new NameController();
 
 nameRouter.get("/name", nameController.findAll);
+nameRouter.get("/name/:id", nameController.findOne);
 nameRouter.post("/name", nameController.create);
+nameRouter.patch("/name/:id", nameController.update);
+nameRouter.delete("/name/:id", nameController.delete);
 
 export default nameRouter;

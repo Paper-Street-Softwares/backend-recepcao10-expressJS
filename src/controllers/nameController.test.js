4a71ff04e77bfb5583dd02a2b0203f38d554f567
Edison Matos <edison.matos@live.com>
2023-12-02 11:32:13 -0300

    fix(jest): Minor test improvements

--- src/controllers/nameController.test.js
+++ src/controllers/nameController.test.js
@@ -11,22 +11,22 @@ describe("Test de nameController", () => {
   it("Verifica se método findOne está definida", () => {
     expect(nameController.findOne).toBeDefined();
   });
-});
 
-it("Verifica se método findAll está definida", () => {
-  expect(nameController.findAll).toBeDefined();
-});
+  it("Verifica se método findAll está definida", () => {
+    expect(nameController.findAll).toBeDefined();
+  });
 
-it("Verifica se método create está definida", () => {
-  expect(nameController.create).toBeDefined();
-});
+  it("Verifica se método create está definida", () => {
+    expect(nameController.create).toBeDefined();
+  });
 
-it("Verifica se método update está definida", () => {
-  expect(nameController.update).toBeDefined();
-});
+  it("Verifica se método update está definida", () => {
+    expect(nameController.update).toBeDefined();
+  });
 
-it("Verifica se método delete está definida", () => {
-  expect(nameController.findOne).toBeDefined();
+  it("Verifica se método delete está definida", () => {
+    expect(nameController.delete).toBeDefined();
+  });
 });
 
 // Teste das requisicoes
@@ -35,6 +35,5 @@ describe("/GET /name findAll()", () => {
   it("Deve localizar todos os modelos na rota", async () => {
     const res = await request(app).get("/name/");
     expect(res.statusCode).toEqual(200);
-    expect(res.body).toHaveProperty([]);
   });
 });

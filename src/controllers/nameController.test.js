df137b74c6a2321adc35ecf90f123549d749a304
EdisonMatos <edison.matos@live.com>
2023-12-03 03:53:50 -0300

    feat(jest): Implemente /GET /name:id tests

--- src/controllers/nameController.test.js
+++ src/controllers/nameController.test.js
@@ -11,39 +11,74 @@ describe("Test de nameController", () => {
   it("Verifica se método findOne está definida", () => {
     expect(nameController.findOne).toBeDefined();
   });
+});
 
-  it("Verifica se método findAll está definida", () => {
-    expect(nameController.findAll).toBeDefined();
-  });
+it("Verifica se método findAll está definida", () => {
+  expect(nameController.findAll).toBeDefined();
+});
 
-  it("Verifica se método create está definida", () => {
-    expect(nameController.create).toBeDefined();
-  });
+it("Verifica se método create está definida", () => {
+  expect(nameController.create).toBeDefined();
+});
 
-  it("Verifica se método update está definida", () => {
-    expect(nameController.update).toBeDefined();
-  });
+it("Verifica se método update está definida", () => {
+  expect(nameController.update).toBeDefined();
+});
 
-  it("Verifica se método delete está definida", () => {
-    expect(nameController.delete).toBeDefined();
-  });
+it("Verifica se método delete está definida", () => {
+  expect(nameController.findOne).toBeDefined();
 });
 
 // Teste das requisicoes
 
 describe("/GET /name findAll()", () => {
-  it("Deve localizar todos os modelos na rota", async () => {
+  it("Retorna statusCode 200", async () => {
     const res = await request(app).get("/name/");
     expect(res.statusCode).toEqual(200);
   });
 });
 
-describe("/GET /name/:id findOne()", () => {
-  it("Deve localizar todos os modelos na rota", async () => {
-    const res = await request(app).get(
-      "/name/74a247a6-dc40-4d48-8368-e41333b35aac"
-    );
+describe("/GET /name:id findOne()", () => {
+  it("Deve retornar statusCode 200", async () => {
+    const res = await request(app)
+      .get("/name/74a247a6-dc40-4d48-8368-e41333b35aac/")
+      .send({
+        name: "jose",
+      });
     expect(res.statusCode).toEqual(200);
+  });
+
+  it("Deve encontrar o usuário pelo id", async () => {
+    const res = await request(app).get(
+      "/name/74a247a6-dc40-4d48-8368-e41333b35aac/"
+    );
     expect(res.body.name).toBe("edison");
+    expect(res.body.id).toBeDefined();
+    expect(res.headers["content-type"]).toEqual(
+      expect.stringContaining("json")
+    );
+  });
+
+  it("Deve retornar o id, nome, updatedAt e createdAt do usuário encontrado", async () => {
+    const res = await request(app)
+      .get("/name/74a247a6-dc40-4d48-8368-e41333b35aac/")
+      .send({
+        name: "jose",
+      });
+    expect(res.body.id).toBeDefined();
+    expect(res.body.name).toBeDefined();
+    expect(res.body.createdAt).toBeDefined();
+    expect(res.body.updatedAt).toBeDefined();
+  });
+
+  it("Deve retornar resposta em json", async () => {
+    const res = await request(app)
+      .get("/name/74a247a6-dc40-4d48-8368-e41333b35aac/")
+      .send({
+        name: "jose",
+      });
+    expect(res.headers["content-type"]).toEqual(
+      expect.stringContaining("json")
+    );
   });
 });

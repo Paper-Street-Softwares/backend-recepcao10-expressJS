4ac373b711264842f868eb99a37cfbc965f5f3ae
EdisonMatos <edison.matos@live.com>
2023-12-04 20:16:23 -0300

    feeat(tests): Crate tests for create()

--- src/controllers/nameController.test.js
+++ src/controllers/nameController.test.js
@@ -11,22 +11,22 @@ describe("Test de nameController", () => {
   it("Verifica se método findOne está definida", () => {
     expect(nameController.findOne).toBeDefined();
   });
-});
 
-it("Verifica se método findAll está definida", () => {
-  expect(nameController.findAll).toBeDefined();
-});
+  it("Verifica se método findAll está definida", () => {
+    expect(nameController.findAll).toBeDefined();
+  });
 
-it("Verifica se método create está definida", () => {
-  expect(nameController.create).toBeDefined();
-});
+  it("Verifica se método create está definida", () => {
+    expect(nameController.create).toBeDefined();
+  });
 
-it("Verifica se método update está definida", () => {
-  expect(nameController.update).toBeDefined();
-});
+  it("Verifica se método update está definida", () => {
+    expect(nameController.update).toBeDefined();
+  });
 
-it("Verifica se método delete está definida", () => {
-  expect(nameController.findOne).toBeDefined();
+  it("Verifica se método delete está definida", () => {
+    expect(nameController.findOne).toBeDefined();
+  });
 });
 
 // Teste das requisicoes
@@ -65,6 +65,48 @@ describe("/GET /name:id findOne()", () => {
       .send({
         name: "jose",
       });
+    expect(res.body.id).toBeDefined();
+    expect(res.body.name).toBeDefined();
+    expect(res.body.createdAt).toBeDefined();
+    expect(res.body.updatedAt).toBeDefined();
+  });
+
+  it("Deve retornar resposta em json", async () => {
+    const res = await request(app)
+      .get("/name/74a247a6-dc40-4d48-8368-e41333b35aac/")
+      .send({
+        name: "jose",
+      });
+    expect(res.headers["content-type"]).toEqual(
+      expect.stringContaining("json")
+    );
+  });
+});
+
+describe("/POST /name create()", () => {
+  it("Deve criar um usuário", async () => {
+    const res = await request(app).post("/name").send({
+      name: "nometeste",
+    });
+    expect(res.body.name).toBe("nometeste");
+    expect(res.body.id).toBeDefined();
+    expect(res.headers["content-type"]).toEqual(
+      expect.stringContaining("json")
+    );
+  });
+
+  it("Deve retornar statusCode 200", async () => {
+    const res = await request(app).post("/name/").send({
+      name: "nometeste",
+    });
+
+    expect(res.statusCode).toEqual(200);
+  });
+
+  it("Deve retornar o id, nome, updatedAt e createdAt do usuário criado", async () => {
+    const res = await request(app).post("/name/").send({
+      name: "anyname",
+    });
     expect(res.body.id).toBeDefined();
     expect(res.body.name).toBeDefined();
     expect(res.body.createdAt).toBeDefined();

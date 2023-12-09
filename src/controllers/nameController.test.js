b4594854842f566f3ff8a653c84036ab874f7cf4
Edison Matos <edison.matos@live.com>
2023-12-09 00:38:58 -0300

    fix(logs): Fix test errors

--- src/controllers/nameController.test.js
+++ src/controllers/nameController.test.js
@@ -1,18 +1,35 @@
 const request = require("supertest");
 const app = require("../app.js");
+const logger = require("../app/logs/logger.js");
 
 const NameController = require("../controllers/nameController.js");
 const { prismaClient } = require("../app/db/prisma/prismaClient.js");
 const nameController = new NameController();
 
 beforeEach(async () => {
-  createUpdateUser = await request(app).post("/name/").send({
-    name: "beforeUpdate",
+  const findBeforeUpdate = await prismaClient.testName.findFirst({
+    where: {
+      name: "beforeUpdate",
+    },
   });
 
-  createToBeDeletedUser = await request(app).post("/name/").send({
-    name: "ToBeDeletedUser",
-  });
+  if (!findBeforeUpdate) {
+    createUpdateUser = await request(app).post("/name/").send({
+      name: "beforeUpdate",
+    });
+
+    const findToBeDeletedUser = await prismaClient.testName.findFirst({
+      where: {
+        name: "ToBeDeletedUser",
+      },
+    });
+
+    if (!findToBeDeletedUser) {
+      createToBeDeletedUser = await request(app).post("/name/").send({
+        name: "ToBeDeletedUser",
+      });
+    }
+  }
 });
 
 afterEach(async () => {

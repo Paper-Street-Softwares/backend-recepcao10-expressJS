fa63c87f4a84fc86eb6e8bc5cbabde2c74a76d8b
EdisonMatos <edison.matos@live.com>
2023-12-10 18:31:41 -0300

    feat(newModel): Update

--- src/controllers/visitaController.js
+++ src/controllers/visitaController.js
@@ -3,7 +3,9 @@ const logger = require("../app/logs/logger.js");
 const { PrismaCliente } = require("../app/db/prisma/prismaClient.js");
 
 class VisitaController {
-  async findAll() {}
+  async findAll() {
+    return response.status(200).json({ msg: "ok" });
+  }
   async findOne() {}
   async create() {}
   async update() {}
--- src/server.js
+++ src/server.js
@@ -2,5 +2,5 @@ const app = require("./app.js");
 const logger = require("./app/logs/logger.js");
 
 app.listen(3000, () => {
-  logger.info("Server ok. Check http://localhost:3000");
+  logger.info("Server ok. Check http://localhost:3000/api/visitantes");
 });

0eecc24419f5c07986655ca248756a10f00ea26b
Edison Matos <edison.matos@live.com>
2023-12-15 17:59:14 -0300

    feat(mongodb): Fix console msg

--- src/server.js
+++ src/server.js
@@ -2,9 +2,5 @@ const app = require("./app.js");
 const logger = require("./app/logs/logger.js");
 
 app.listen(3000, () => {
-  logger.info({
-    ServerStatus: "Ok",
-    visitanteRoute: "http://localhost:3000/api/visitantes",
-    visitasRoute: "http://localhost:3000/api/visitas",
-  });
+  logger.info("Server ok");
 });

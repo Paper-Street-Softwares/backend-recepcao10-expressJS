2324636776d4edacd63b90a61462c2a8a9721208
Edison Matos <edison.matos@live.com>
2023-12-19 09:22:00 -0300

    feat(swagger): Implement swagger

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+const swaggerUi = require("swagger-ui-express");
+const swaggerFile = require("../swagger-output.json");
 const express = require("express");
 const router = require("./routers/routes.js");
 
@@ -5,6 +7,8 @@ const app = express();
 
 app.use(express.json());
 app.use(router);
+
+app.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.get("/", (request, response) => {
   response.status(200).send({ status: "Server OK." });

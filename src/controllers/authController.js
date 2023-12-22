7632cea0a7cde2ba1c19644aeb4838cb399836b6
Edison Matos <edison.matos@live.com>
2023-12-22 19:17:15 -0300

    feat(auth): Create compare password without bcrypt. Missing tests

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -5,8 +5,23 @@ const logger = require("../app/logs/logger.js");
 class AuthController {
   async login(request, response) {
     try {
-      console.log("Test");
-      return response.status(200).json({ test: "test" });
+      const { email, password } = request.body;
+
+      const findUser = await prismaClient.user.findFirst({
+        where: {
+          email,
+        },
+      });
+
+      if (password === findUser.password) {
+        return response.status(200).json({
+          status: "Usuário autenticado com sucesso!",
+          tokenJWT:
+            "$2b$12$u1qZPiqlqkMbhc/0/oepCO/41d.WslEbQfPr9rVV0Eh.ZDvgVEsAS$2b$12$u1qZPiqlqkMbhc/0/oepCO/41d.WslEbQfPr9rVV0Eh.ZDvgVEsAS$2b$12$u1qZPiqlqkMbhc/0/oepCO/41d.WslEbQfPr9rVV0Eh.ZDvgVEsAS$2b$12$u1qZPiqlqkMbhc/0/oepCO/41d.WslEbQfPr9rVV0Eh.ZDvgVEsAS$2b$12$u1qZPiqlqkMbhc/0/oepCO/41d.WslEbQfPr9rVV0Eh.ZDvgVEsAS$2b$12$u1qZPiqlqkMbhc/0/oepCO/41d.WslEbQfPr9rVV0Eh.ZDvgVEsAS",
+        });
+      } else {
+        return response.status(400).json({ status: "Falha na autenticação" });
+      }
     } catch (error) {
       logger.error(error);
       return response.status(500).json({ error: error.message });

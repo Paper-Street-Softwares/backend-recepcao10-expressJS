7da684866d043063ed02f4019bf2f21d7ef55e69
Edison Matos <edison.matos@live.com>
2023-12-22 20:05:38 -0300

    feat(auth): Implement test for login()

--- src/controllers/tests/authController.test.js
+++ src/controllers/tests/authController.test.js
@@ -1,9 +1,31 @@
 const request = require("supertest");
 const app = require("../../app.js");
+const { prismaClient } = require("../../app/db/prisma/prismaClient.js");
+
+beforeAll(async () => {
+  const createTestUser = await prismaClient.user.create({
+    data: {
+      name: "testUserForAuth",
+      email: "authtest@authtest.com",
+      password: "123456",
+    },
+  });
+});
+
+afterAll(async () => {
+  const deleteTestUser = await prismaClient.user.deleteMany({
+    where: {
+      name: "testUserForAuth",
+    },
+  });
+});
 
 describe("/POST /auth login()", () => {
   test("Deve autenticar usuário e retornar status 200", async () => {
-    const res = await request(app).post("/auth/");
+    const res = await request(app).post("/auth/").send({
+      email: "authtest@authtest.com",
+      password: "123456",
+    });
 
     expect(res.statusCode).toEqual(200);
   });

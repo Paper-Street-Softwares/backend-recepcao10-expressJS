eed1245af5a40c0ca981529fd812a7dbfa7e5fef
Edison Matos <edison.matos@live.com>
2023-12-26 09:30:10 -0300

    fix(auth): Fix error on mismatch credentials

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -15,27 +15,31 @@ class AuthController {
         },
       });
 
-      const userID = findUser.id;
+      if (findUser) {
+        const userID = findUser.id;
 
-      const token = jwt.sign({ userID }, process.env.SECRET_JWT, {
-        expiresIn: 86400,
-      });
+        const token = jwt.sign({ userID }, process.env.SECRET_JWT, {
+          expiresIn: 86400,
+        });
 
-      bcrypt.compare(password, findUser.password, (err, result) => {
-        if (err) {
-          console.log(password);
-          console.log(findUser.password);
-          console.log("Error ao comparar senhas.", err);
-          return response.status(500).send({ error: err.message });
-        }
+        bcrypt.compare(password, findUser.password, (err, result) => {
+          if (err) {
+            console.log(password);
+            console.log(findUser.password);
+            console.log("Error ao comparar senhas.", err);
+            return response.status(500).send({ error: err.message });
+          }
 
-        if (result) {
-          response.header("Authorization", `Bearer ${token}`);
-          response.status(200).send({ status: "Autenticado com sucesso." });
-        } else {
-          response.status(400).json({ error: "Falha na autenticação." });
-        }
-      });
+          if (result) {
+            response.header("Authorization", `Bearer ${token}`);
+            response.status(200).send({ status: "Autenticado com sucesso." });
+          } else {
+            response.status(400).json({ error: "Email ou senha inválidos." });
+          }
+        });
+      } else {
+        response.status(400).json({ error: "Email ou senha inválidos." });
+      }
     } catch (error) {
       logger.error(error);
       return response.status(500).json({ error: error.message });
